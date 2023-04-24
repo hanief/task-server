3667764cb9ed6263809bf1853d2b3f464aa390dc
Hanief <hanief@gmail.com>
2023-04-24 16:14:40 +0700

    Fix error

---
+++ README.md
@@ -0,0 +1,5 @@
+# Task Server
+
+# Setup
+1. Run `yarn`
+2. Run `yarn serve`
--- index.js
+++ index.js
@@ -1,50 +1,63 @@
-const environment = process.argv.slice(2) ? process.argv.slice(2)[0] : 'db'
-const port = process.argv.slice(3) ? process.argv.slice(3)[0] : 3001
+const environment = process.argv.slice(2).length ? process.argv.slice(2)[0] : 'db'
+const port = process.argv.slice(3).length ? process.argv.slice(3)[0] : 3001
 
-const jsonServer = require("json-server");
-const secretKey = 'y7KjrBFOgLRz7mTPvwkue2CTCrUH4TJ20g55cVyWXvw'
-const server = jsonServer.create();
-const router = jsonServer.router(`${environment}.json`);
-const middlewares = jsonServer.defaults();
+const fs = require('fs');
+require('dotenv').config()
+const secretKey = process.env.SECRET_KEY
+const mode = environment === 'dev' ? fs.constants.COPYFILE_EXCL : null
 
-server.use(middlewares);
+fs.copyFile('db.json', `${environment}.json`, mode, (err) => {
+  if (err) {
+    if (err.code === 'EEXIST') {
+      console.log(`Using existing ${environment}.json file`);
+      return runServer()
+    } else {
+      throw err;
+    }
+  }
 
-server.get("/", (req, res) => {
-  res.send("Hello World!");
+  runServer()
 });
 
-function isAuthorized(req) {
-  if (req.headers.authorization) {
-    const [type, credentials] = req.headers.authorization.split(' ')
-    if (type === 'Bearer' && credentials === secretKey) {
-      return true
+function runServer() {
+  const jsonServer = require("json-server");
+  const server = jsonServer.create();
+  const router = jsonServer.router(`${environment}.json`);
+  const middlewares = jsonServer.defaults();
+
+  server.use(middlewares);
+
+  function isAuthorized(req) {
+    if (req.headers.authorization) {
+      const [type, credentials] = req.headers.authorization.split(' ')
+      if (type === 'Bearer' && credentials === secretKey) {
+        return true
+      }
     }
+
+    return false
   }
 
-  return false
-}
-
-server.use((req, res, next) => {
-  if (isAuthorized(req)) {
-    if (req.path === '/tasks') {
-      if (req.query.user_id) {
-        next()
+  server.use((req, res, next) => {
+    if (isAuthorized(req)) {
+      if (req.path.substring(0, 6) === '/tasks') {
+        if (req.query.user_id) {
+          next()
+        } else {
+          if (req.method === 'POST') {
+            res.json([])
+          } else {
+            res.json({})
+          }
+        }
       } else {
-        res.json([])
-      }
-    } else if (req.path.substring(0, 7) === '/tasks/') {
-      if (req.query.user_id) {
         next()
-      } else {
-        res.json({})
       }
     } else {
-      next()
+      res.sendStatus(401)
     }
-  } else {
-    res.sendStatus(401)
-  }
-})
+  })
 
-server.use(router);
-server.listen(port, () => console.log("API Server is running"));
+  server.use(router);
+  server.listen(port, () => console.log(`Server ${environment} is running`));
+}
