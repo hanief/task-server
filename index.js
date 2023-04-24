d35c686a7e1e1e0f7e82f2f75e537690fe14cfe0
Hanief <hanief@gmail.com>
2023-04-24 13:27:38 +0700

    Fix

--- index.js
+++ index.js
@@ -1,27 +1,50 @@
-const jsonServer = require("json-server");
+const environment = process.argv.slice(2)[0] === 'test' ? 'test' : 'dev'
+const port = process.argv.slice(3) ? process.argv.slice(3)[0] : 3001
 
+const jsonServer = require("json-server");
+const secretKey = process.env.NUXT_PUBLIC_SECRET
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router(filePath, { watch: environment === 'dev' });
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 
-server.get('/', (req, res) => {
-  res.send('Hello!')
-})
+server.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+function isAuthorized(req) {
+  if (req.headers.authorization) {
+    const [type, credentials] = req.headers.authorization.split(' ')
+    if (type === 'Bearer' && credentials === secretKey) {
+      return true
+    }
+  }
+
+  return false
+}
 
 server.use((req, res, next) => {
-  if (req.path === '/tasks') {
-    if (req.query.userId) {
-      next()
+  if (isAuthorized(req)) {
+    if (req.path === '/tasks') {
+      if (req.query.user_id) {
+        next()
+      } else {
+        res.json([])
+      }
+    } else if (req.path.substring(0, 7) === '/tasks/') {
+      if (req.query.user_id) {
+        next()
+      } else {
+        res.json({})
+      }
     } else {
-      res.json([])
+      next()
     }
   } else {
-    next()
+    res.sendStatus(401)
   }
 })
 
 server.use(router);
-
-server.listen(3001, () => console.log("Server is running"));
+server.listen(port, () => console.log("API Server is running"));
